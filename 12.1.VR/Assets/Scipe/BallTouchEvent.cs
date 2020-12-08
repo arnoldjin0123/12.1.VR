3cd585b246b7700b1944749ae0e14dc32457aca0
arnoldjin0123 <71679939+arnoldjin0123@users.noreply.github.com>
2020-12-08 11:24:29 +0800

    Build exe
    finish scence

---
+++ 12.1.VR/Assets/Scipe/BallTouchEvent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BallTouchEvent : MonoBehaviour
+{
+    public Text ScoreT;
+    int scoreV=0;
+    void OnTriggerEnter(Collider col)
+    {
+        switch (col.tag)
+        {
+            case "score10":
+                scoreV += 10;
+                ScoreT.text = "score" + scoreV;
+                break;
+            case "score20":
+                scoreV += 20;
+                ScoreT.text = "score" + scoreV;
+                break;
+            case "score30":
+                scoreV += 30;
+                ScoreT.text = "score" + scoreV;
+                break;
+            case "score40":
+                scoreV += 40;
+                ScoreT.text = "score" + scoreV;
+                break;
+            case "reset":
+                SceneManager.LoadScene("level1");
+                break;
+        }
+    }
+}
